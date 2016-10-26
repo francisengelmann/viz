e0d76403a5c803d4517d336665307d3044dfda45
Francis Engelmann <francis.engelmann@gmail.com>
2016-10-26 09:10:30 +0200

    updated viz.h

--- include/viz/viz.h
+++ include/viz/viz.h
@@ -16,7 +16,7 @@
 #include <eigen/Dense>
 
 // Own includes
-//#include "geometry.h"
+#include "geometry.h"
 
 class vtkRenderer;
 class vtkRenderWindow;
@@ -29,35 +29,73 @@ class Visualization
 {
 public:
 
+  // -----------------------------------------------------------------------------------------------
+  // Viewer Functions
+  // -----------------------------------------------------------------------------------------------
   Visualization(int width=1200, int height=300, int renderers_count=1, bool vertical_split=true);
   void setActiveRenderer(int rendererId);
   void connectCameras(int i, int j);
-
+  void setBackgroundColor(const Eigen::Vector3f& color);
+  void setBackgroundColor(float r, float g, float b);
+  void setWindowSize(const int width, const int height);
+  void hideAllActors();
   void show();
   void draw();
+
+  // -----------------------------------------------------------------------------------------------
+  // Render Functions
+  // -----------------------------------------------------------------------------------------------
+  void renderImage(cv::Mat &image);
+  void renderDepth(cv::Mat& depth, bool flipped=true);
+
+  // -----------------------------------------------------------------------------------------------
+  // Camera Functions
+  // -----------------------------------------------------------------------------------------------
+  void setK(const Eigen::Matrix3d& K, const int width, const int height);
+  void setCameraPosition(const Eigen::Vector3d& pos, double dist);
+  void setCameraPosition(const Eigen::Vector3d& pos);
+  void setCameraFocalPoint(const Eigen::Vector3d& fp);
+  void printCameraPosition(void);
+  void setNearClippingRange(double d);
+  void takeScreenshot2(const std::string& path);
+  void takeScreenshot(const std::string& path, int width=900, int height=900);
+  void takeMovie(const std::string& path,
+                 const std::vector<Eigen::Vector3d>& waypoints,
+                 int width=900, int height=900);
+  void takeMovie2(const std::string& path,
+                 const std::vector<Eigen::Vector3d>& waypoints,
+                 const std::vector<Eigen::Vector3d>& focalpoints,
+                 const std::vector<double>& speeds,
+                 int width=900, int height=900);
+  void pointToOrigin();
+  void pointToScene();
+  void pointFromTop();
+  void lookFromAt(Eigen::Vector3d from, Eigen::Vector3d at);
+  void enableParallelProjection(void);
+
+  // -----------------------------------------------------------------------------------------------
+  // Add Functions
+  // -----------------------------------------------------------------------------------------------
   void addGrid(double depth_min = 5,
                double depth_max = 30,
                double width_min = -6,
                double width_max = 6,
                double y = 1.5);
-
   void addGridSmall(void);
-
-  void setBackgroundColor(const Eigen::Vector3f& color);
-  void setBackgroundColor(float r, float g, float b);
   void addAxes();
   void addAxesBig();
-  /*void addSegment(const Eigen::Vector3d start,
-                  const Eigen::Vector3d end,
-                  const Eigen::Vector3f color);*/
   void addTrajectory(const std::vector<Eigen::Vector3d>& trajectory, const Eigen::Vector3f& color);
   void addFrustum(const Eigen::Matrix4d& pose);
-  void addPointcloud(const std::shared_ptr<viz::Pointcloud>& pointcloud, double point_size = 3);
-  void addPointcloud(const viz::Pointcloud& pointcloud, double point_size = 3);
+  void addPointcloud(const std::vector<Eigen::Vector3d>& vertices,
+                     const std::vector<Eigen::Vector3f>& colors,
+                     double point_size = 3);
   void addPlane(const double a, const double b, const double c, const double d);
   void addPlane(const Eigen::Vector4d& plane);
   void addPLYModel(const std::string& path);
-  void addBoundingBox(const viz::BoundingBox& bounding_box);
+  void addBoundingBox(const Eigen::Vector3d& size,
+                      const Eigen::Vector3d& translation,
+                      const double rotation_y,
+                      const Eigen::Vector3f& color);
   void addNormals(const cv::Mat& vertices, const cv::Mat& normals);
   void addNormals(const std::vector<Eigen::Vector3d>& vertices,
                   const std::vector<Eigen::Vector3d>& normals);
@@ -70,7 +108,6 @@ public:
                               double isoValue = 0,
                               double opacity = 0.1,
                               double lineWidth = 1.0);
-  void enableParallelProjection(void);
   void addGridSamples(const Eigen::MatrixXd& values,
                               const Eigen::Matrix4d& pose,
                               const Eigen::Vector3f& color,
@@ -79,47 +116,18 @@ public:
   void addPlaneCut(const Eigen::MatrixXd &values,
                    vtkPlane* plane, bool legend=true);
   void addVolumeRenderer(const Eigen::MatrixXd& values);
-  //void addAnnotation(const gvl::Annotation& annotation,
-  //                   std::string& models_path);
   void addText(const std::string& text, const Eigen::Vector3d& translation, const Eigen::Vector3f& color = Eigen::Vector3f(0,0,0));
   void addText2d(const std::string& text, double pos_x = 30, double pos_y = 30, const Eigen::Vector3f& color = Eigen::Vector3f(0,0,0));
   void addCallbackCommand(vtkSmartPointer<vtkCallbackCommand> keypressCallback);
   void addCallbackCommand(void (*f)(vtkObject *caller, unsigned long eid,
                           void *clientdata, void *calldata));
-  void renderImage(cv::Mat &image);
-  void renderDepth(cv::Mat& depth, bool flipped=true);
-  void setK(const Eigen::Matrix3d& K, const int width, const int height);
 
-  void setCameraPosition(const Eigen::Vector3d& pos, double dist);
-  void setCameraPosition(const Eigen::Vector3d& pos);
-  void setCameraFocalPoint(const Eigen::Vector3d& fp);
-  void setWindowSize(const int width, const int height);
-  void printCameraPosition(void);
-  void setNearClippingRange(double d);
-
-  void takeScreenshot2(const std::string& path);
-
-  void takeScreenshot(const std::string& path, int width=900, int height=900);
-
-  void takeMovie(const std::string& path,
-                 const std::vector<Eigen::Vector3d>& waypoints,
-                 int width=900, int height=900);
-
-  void takeMovie2(const std::string& path,
-                 const std::vector<Eigen::Vector3d>& waypoints,
-                 const std::vector<Eigen::Vector3d>& focalpoints,
-                 const std::vector<double>& speeds,
-                 int width=900, int height=900);
-  void pointToOrigin();
-  void pointToScene();
-  void pointFromTop();
-  void lookFromAt(Eigen::Vector3d from, Eigen::Vector3d at);
-  void hideAllActors();
-
+  // -----------------------------------------------------------------------------------------------
+  // Variables
+  // -----------------------------------------------------------------------------------------------
   vtkRenderer* renderer;
   vtkRenderWindow* renderWindow;
   vtkRenderWindowInteractor* renderWindowInteractor;
-
   int render_id_current;
   std::vector<vtkRenderer*> renderers;
 private:
