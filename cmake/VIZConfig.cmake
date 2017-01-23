bbda7a370696aebd5be191b6ea427ac2a4fa87b4
Francis Engelmann <engelmann@vision.rwth-aachen.de>
2017-01-23 12:06:53 +0100

    Fix cmake pathes

--- cmake/VIZConfig.cmake
+++ cmake/VIZConfig.cmake
@@ -1,7 +1,7 @@
 include("${CMAKE_CURRENT_LIST_DIR}/VIZTargets.cmake")
 
-find_path(VIZ_INCLUDE_DIR "viz" HINTS "${CMAKE_SOURCE_DIR}/include/" )
-find_library(VIZ_LIBRARY NAMES VIZ HINTS "${CMAKE_SOURCE_DIR}/lib/")
+find_path(VIZ_INCLUDE_DIR "viz" HINTS "${CMAKE_CURRENT_LIST_DIR}/../include/" )
+find_library(VIZ_LIBRARY NAMES VIZ HINTS "${CMAKE_CURRENT_LIST_DIR}/../lib/")
 
 set(VIZ_LIBRARIES ${VIZ_LIBRARY} )
 set(VIZ_INCLUDE_DIRS ${VIZ_INCLUDE_DIR} )
