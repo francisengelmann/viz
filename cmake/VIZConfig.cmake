3842bc7994dae4223c64308c28d1d0d4d23cb636
Francis Engelmann <engelmann@vision.rwth-aachen.de>
2017-01-23 12:20:12 +0100

    Add debug info for cmake

--- cmake/VIZConfig.cmake
+++ cmake/VIZConfig.cmake
@@ -5,3 +5,7 @@ find_library(VIZ_LIBRARY NAMES VIZ HINTS "${CMAKE_CURRENT_LIST_DIR}/../lib/")
 
 set(VIZ_LIBRARIES ${VIZ_LIBRARY} )
 set(VIZ_INCLUDE_DIRS ${VIZ_INCLUDE_DIR} )
+
+message(STATUS "VIZ found")
+message(STATUS "  includes: " ${VIZ_INCLUDE_DIRS})
+message(STATUS "  libs: " ${VIZ_LIBRARIES} )
