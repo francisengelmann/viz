944a5e3ad6bd0c18133ad9cee098ae366ee21061
Francis Engelmann <francis.engelmann@gmail.com>
2017-04-25 12:00:42 +0200

    Fix typo

--- src/viz.h
+++ src/viz.h
@@ -71,7 +71,7 @@ public:
   void pointToScene();
   void pointFromTop();
   void lookFromAt(Eigen::Vector3d from, Eigen::Vector3d at);
-  void setLookUp(Eigen::Vector3d v);
+  void setViewUp(Eigen::Vector3d v);
   void enableParallelProjection(void);
 
   // -----------------------------------------------------------------------------------------------
